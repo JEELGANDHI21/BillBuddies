b9f4f75fb998e00855631b412312b44ceafef46a
Jeel215 <jeelgandhi4585@gmail.com>
2025-05-13 22:08:55 +0530

    first commit with Header and Database initialised with convex and authenticated with Clerk

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="pt-16">Jeel Gandhi
+      <Button variant={'destructive'}>Here</Button>
+    </div>
+  );
+}
