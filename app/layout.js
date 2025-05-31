8e95db412394b862583c8dc612596a8deed2bfee
Jeel215 <jeelgandhi4585@gmail.com>
2025-05-31 16:30:48 +0530

    Fixed Title and Icon

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,7 @@ import { Toaster } from "sonner";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Splitr",
+  title: "BillBuddies",
   description: "The smartest way to split expenses with friends",
 };
 
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/logos/logo-s.png" sizes="any" />
+        <link rel="icon" href="/logos/svgviewer-output (1).svg" sizes="any" />
       </head>
       <body className={`${inter.className}`}>
         <ClerkProvider >
